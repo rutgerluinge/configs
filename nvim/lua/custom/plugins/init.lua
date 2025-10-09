71b5a636d49d4eb2a24c9b8bc63e78a7607eba17
Rutger luinge <r.luinge@researchable.nl>
2025-10-09 15:01:47 +0200

    nvim

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,15 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  -- File explorer
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('nvim-tree').setup()
+      vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
+    end,
+  },
+}
